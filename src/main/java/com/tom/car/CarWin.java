451cda52b73b4c2630ba487824f120f4b1538ae1
王冠棋 <coco82401@gmail.com>
2021-06-01 14:58:03 +0800

    6/1 14:57

---
+++ src/main/java/com/tom/car/CarWin.java
@@ -0,0 +1,15 @@
+package com.tom.car;
+
+import javax.swing.*;
+
+public class CarWin {
+    static final int WIDTH = 600 ;
+    static final int HEIGHT=400 ;
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(WIDTH,HEIGHT);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
